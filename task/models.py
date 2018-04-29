c4f1e2ee5e75e6aec487facc0ab4bbdab9ac787c
Mikhail Makarov <mikhail@makarov.us>
2018-04-29 10:46:27 +0500

    Add two methods for Category model

--- task/models.py
+++ task/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Category(models.Model):
@@ -21,3 +22,41 @@ class Category(models.Model):
             pk=self.pk,
             name=self.name,
         )
+
+    @staticmethod
+    def add_new_category(name, parent=None, children=[]):
+        rows_processed = 1
+        category, created = Category.objects.get_or_create(name=name)
+        if not created and category.parent and category.parent != parent:
+            category.try_rename_or_remove()
+            category = Category(name=name)
+        category.parent = parent
+        category.save()
+
+        children_names = []
+        for child in children:
+            rows_processed += Category.add_new_category(parent=category, **child)
+            children_names.append(child['name'])
+
+        # Remove parent category for child which not given as a child of a category
+        for child in Category.objects.filter(parent=category):
+            if child.name not in children_names:
+                child.parent = None
+                child.save()
+
+        return rows_processed
+
+    def try_rename_or_remove(self):
+        old_name = self.name
+        for x in range(3):
+            try:
+                self.name = '{date} Removed - {old_name}'.format(
+                    date=timezone.now(),
+                    old_name=old_name,
+                )[:255]
+                self.parent = None
+                self.save()
+                return
+            except Exception:
+                continue
+        self.delete()
